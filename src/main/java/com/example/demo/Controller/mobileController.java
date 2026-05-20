17ea55e3905349683cdc846d49e7bb4f5cac55c6
rushikpatel08 <rushikpatel08@gmail.com>
2026-05-20 09:31:20 -0400

    Jenkinsfile changed

--- src/main/java/com/example/demo/Controller/mobileController.java
+++ src/main/java/com/example/demo/Controller/mobileController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/mobile")
-@CrossOrigin(origins = "http://34.204.75.205:80")
+@CrossOrigin(origins = "http://34.204.75.205")
 public class mobileController {
 
 
