257f4188b982b5d0b444fea260ca79f8008b52f1
rushikpatel08 <rushikpatel08@gmail.com>
2026-05-20 12:13:36 -0400

    Jenkinsfile changed

--- src/main/java/com/example/demo/Security/CorsConfig.java
+++ src/main/java/com/example/demo/Security/CorsConfig.java
@@ -8,16 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://34.204.75.205")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(false);
+                        .allowedHeaders("*");
             }
         };
     }
